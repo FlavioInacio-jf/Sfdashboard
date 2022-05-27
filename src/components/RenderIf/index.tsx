098ab7fc6e359746b2b5e039321b1d1b6353d4ba
https://gitlab.com/FlavioInacio-jf <jflavioinacio22@gmail.com>
2022-05-27 14:12:11 -0300

    feat: Criado o componente auxiliar para lidar com casos booleanos

---
+++ src/components/RenderIf/index.tsx
@@ -0,0 +1,10 @@
+import { FC, ReactNode } from 'react';
+
+interface RenderIfProps {
+  children: ReactNode;
+  condition: boolean;
+}
+
+export const RenderIf: FC<RenderIfProps> = ({ children, condition }) => {
+  return <>{condition ? { children } : null}</>;
+};

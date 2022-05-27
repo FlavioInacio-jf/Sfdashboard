d61fcefde269a8b7778ece3c4fb2c02c9437d4b0
https://gitlab.com/FlavioInacio-jf <jflavioinacio22@gmail.com>
2022-05-27 00:27:23 -0300

    chore: Configurado o react query na aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import { FC } from 'react';
+import { QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import { Routes } from './routes';
+import { queryClient } from './services/query';
 import GlobalStyle from './styles/globalStyle';
 import { Theme } from './styles/Theme';
 
@@ -7,7 +10,10 @@ export const App: FC = () => {
   return (
     <Theme>
       <GlobalStyle />
-      <Routes />
+      <QueryClientProvider client={queryClient}>
+        <Routes />
+        <ReactQueryDevtools initialIsOpen />
+      </QueryClientProvider>
     </Theme>
   );
 };

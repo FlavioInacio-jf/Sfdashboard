bfedc100588210ee658a4423d6352ee738075a54
https://gitlab.com/FlavioInacio-jf <jflavioinacio22@gmail.com>
2022-05-28 21:42:42 -0300

    feat: Adicionado uma mensagem na pagina 404.

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -1,5 +1,16 @@
 import { FC } from 'react';
+import notFoundImg from '../../assets/not_found.svg';
+import { Message } from '../../components/Message';
+import { Custom404Container } from './styles';
 
 export const Custom404: FC = () => {
-  return <div>404</div>;
+  return (
+    <Custom404Container>
+      <Message
+        title="Resource not found"
+        description="Please, Check that the address provided is correct."
+        image={notFoundImg}
+      />
+    </Custom404Container>
+  );
 };

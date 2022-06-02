4e843f25bd5ff9be2be60fd8f30d5a03d3ff619b
https://gitlab.com/FlavioInacio-jf <jflavioinacio22@gmail.com>
2022-06-02 03:10:15 -0300

    feat: Excluido a pagina de stocks

--- src/pages/Stocks/index.tsx
+++
@@ -1,5 +0,0 @@
-import { FC } from 'react';
-
-export const Stocks: FC = () => {
-  return <div>Stocks</div>;
-};
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,7 +4,6 @@ import { Custom404 } from '../pages/404';
 import { Dashboard } from '../pages/Dashboard';
 import { Login } from '../pages/Login';
 import { Register } from '../pages/Register';
-import { Stocks } from '../pages/Stocks';
 import { PrivateRoute } from './PrivateRoute';
 
 export const Routes = () => {
@@ -28,7 +27,6 @@ export const Routes = () => {
           }
         />
         <Route path="/products" element={<PrivateRoute page={Dashboard} />} />
-        <Route path="/stocks" element={<PrivateRoute page={Stocks} />} />
         <Route path="*" element={<Custom404 />} />
       </RoutesRouterDom>
     </BrowserRouter>

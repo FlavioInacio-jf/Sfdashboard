167cd7e930fc46321eb09ecd18d81915068c5bcf
https://gitlab.com/FlavioInacio-jf <jflavioinacio22@gmail.com>
2022-06-15 20:34:20 -0300

    feat: Criado a rota de pegar comentarios de um produto

--- backend/src/routes/comments/index.ts
+++ backend/src/routes/comments/index.ts
@@ -1,16 +1,20 @@
 import { Router } from "express";
 import { CreateCommentController } from "../../controllers/comments/CreateCommentController";
 import { DeleteCommentController } from "../../controllers/comments/DeleteCommentController";
+import { GetAllProductCommentsController } from "../../controllers/comments/GetAllProductCommentsController";
 import { EnsureAuthenticated } from "../../middlewares/EnsureAuthenticated";
 
 const commentsRoutes = Router();
 
+const getAllProductCommentsController = new GetAllProductCommentsController();
 const createCommentController = new CreateCommentController();
 const deleteCommentController = new DeleteCommentController();
+
 const ensureAuthenticated = new EnsureAuthenticated();
 
 commentsRoutes.use(ensureAuthenticated.execute);
 
+commentsRoutes.get("/:product_id", getAllProductCommentsController.execute);
 commentsRoutes.post("/", createCommentController.execute);
 commentsRoutes.delete("/:id", deleteCommentController.execute);
 

bd4f21db00d0b28dea4e9e2d99638c6e084d298d
https://gitlab.com/FlavioInacio-jf <jflavioinacio22@gmail.com>
2022-06-15 19:55:53 -0300

    feat: Adicionado a rota de excluir comentario.

--- backend/src/routes/comments/index.ts
+++ backend/src/routes/comments/index.ts
@@ -1,13 +1,17 @@
 import { Router } from "express";
 import { CreateCommentController } from "../../controllers/comments/CreateCommentController";
+import { DeleteCommentController } from "../../controllers/comments/DeleteCommentController";
 import { EnsureAuthenticated } from "../../middlewares/EnsureAuthenticated";
 
 const commentsRoutes = Router();
+
 const createCommentController = new CreateCommentController();
+const deleteCommentController = new DeleteCommentController();
 const ensureAuthenticated = new EnsureAuthenticated();
 
 commentsRoutes.use(ensureAuthenticated.execute);
 
 commentsRoutes.post("/", createCommentController.execute);
+commentsRoutes.delete("/:id", deleteCommentController.execute);
 
 export { commentsRoutes };

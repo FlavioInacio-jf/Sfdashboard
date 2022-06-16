a5ec65bf5d63ab1c0e28123b1cdcc038c122c59c
https://gitlab.com/FlavioInacio-jf <jflavioinacio22@gmail.com>
2022-06-16 09:43:04 -0300

    feat: Adicionado esquema de validação na rota de endereços

--- backend/src/routes/addresses/index.ts
+++ backend/src/routes/addresses/index.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { CreateAddressController } from "../../controllers/addresses/CreateAddressController";
 import { EnsureAuthenticated } from "../../middlewares/EnsureAuthenticated";
+import { validateResource } from "../../middlewares/validateResource";
+import { createSchema } from "./schemas";
 
 const addressesRoutes = Router();
 const createAddressController = new CreateAddressController();
@@ -8,6 +10,10 @@ const createAddressController = new CreateAddressController();
 const ensureAuthenticated = new EnsureAuthenticated();
 addressesRoutes.use(ensureAuthenticated.execute);
 
-addressesRoutes.post("/", createAddressController.execute);
+addressesRoutes.post(
+  "/",
+  validateResource(createSchema),
+  createAddressController.execute,
+);
 
 export { addressesRoutes };

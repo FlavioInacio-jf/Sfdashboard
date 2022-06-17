472ddc1fb8c0a3a0918f6f9e07442b61bcfdf392
https://gitlab.com/FlavioInacio-jf <jflavioinacio22@gmail.com>
2022-06-17 16:38:34 -0300

    feat: Criado a rota de atualizar endereço

--- backend/src/routes/addresses/index.ts
+++ backend/src/routes/addresses/index.ts
@@ -1,13 +1,15 @@
 import { Router } from "express";
 import { CreateAddressController } from "../../controllers/addresses/CreateAddressController";
 import { DeleteAddressController } from "../../controllers/addresses/DeleteAddressController";
+import { UpdateAddressController } from "../../controllers/addresses/UpdateAddressController";
 import { EnsureAuthenticated } from "../../middlewares/EnsureAuthenticated";
 import { validateResource } from "../../middlewares/validateResource";
-import { createSchema } from "./schemas";
+import { createSchema, updateSchema } from "./schemas";
 
 const addressesRoutes = Router();
 const createAddressController = new CreateAddressController();
 const deleteAddressController = new DeleteAddressController();
+const updateAddressController = new UpdateAddressController();
 
 const ensureAuthenticated = new EnsureAuthenticated();
 addressesRoutes.use(ensureAuthenticated.execute);
@@ -16,6 +18,11 @@ addressesRoutes.post(
   "/",
   validateResource(createSchema),
   createAddressController.execute,
+);
+addressesRoutes.patch(
+  "/:id",
+  validateResource(updateSchema),
+  updateAddressController.execute,
 );
 addressesRoutes.delete("/:id", deleteAddressController.execute);
 

5ab9f83398893faad6745d86e67202c68cfb72d4
https://gitlab.com/FlavioInacio-jf <jflavioinacio22@gmail.com>
2022-06-17 16:46:49 -0300

    feat: Criado a rota de pegar todos os endereços do usuário.

--- backend/src/routes/addresses/index.ts
+++ backend/src/routes/addresses/index.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import { CreateAddressController } from "../../controllers/addresses/CreateAddressController";
 import { DeleteAddressController } from "../../controllers/addresses/DeleteAddressController";
+import { GetAllAddressesController } from "../../controllers/addresses/GetAllAddressesController";
 import { UpdateAddressController } from "../../controllers/addresses/UpdateAddressController";
 import { EnsureAuthenticated } from "../../middlewares/EnsureAuthenticated";
 import { validateResource } from "../../middlewares/validateResource";
 import { createSchema, updateSchema } from "./schemas";
 
 const addressesRoutes = Router();
+const getAllAddressesController = new GetAllAddressesController();
 const createAddressController = new CreateAddressController();
 const deleteAddressController = new DeleteAddressController();
 const updateAddressController = new UpdateAddressController();
@@ -14,6 +16,7 @@ const updateAddressController = new UpdateAddressController();
 const ensureAuthenticated = new EnsureAuthenticated();
 addressesRoutes.use(ensureAuthenticated.execute);
 
+addressesRoutes.get("/", getAllAddressesController.execute);
 addressesRoutes.post(
   "/",
   validateResource(createSchema),
